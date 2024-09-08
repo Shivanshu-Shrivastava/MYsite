b78b984814ea0b67e0383005a4509e700cfa02a1
Shivanshu Shrivastava <74667017+Shivanshu-Shrivastava@users.noreply.github.com>
2024-09-08 17:08:28 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,16 +15,16 @@ function About() {
                 <div className='col-md-6 mx-auto  col-sm-12'>
                     <div className='d-flex pt-md-4  px-md-4 p-0 flex-column'>
                         <p className='text-justify'>
-                             Hello! I am Shivanshu Shrivastava, a B.Tech student specializing in Automobile Engineering at <strong>Madhav Institute of Technology & Science</strong>.<br>
+                             Hello! I am Shivanshu Shrivastava, a B.Tech student specializing in Automobile Engineering at <strong>Madhav Institute of Technology & Science</strong>.<br />
     I completed my higher education at <strong>Time School</strong> and have gained valuable experience through three internships with reputable companies.</p>
     
-    <p>In my first internship as a Front-End Developer, I worked on designing CV builder and job portal websites.<br>
+    <p>In my first internship as a Front-End Developer, I worked on designing CV builder and job portal websites.<br />
     This experience significantly enhanced my coding skills, especially in writing well-documented code and improving time management.</p>
     
-    <p>During my second internship as a Full-Stack Developer, I developed an email sender application using the <code>Nodemailer</code> package.<br>
+    <p>During my second internship as a Full-Stack Developer, I developed an email sender application using the <code>Nodemailer</code> package.<br />
     Though short in duration, this internship further solidified my backend development skills.</p>
     
-    <p>My third internship was particularly exciting, as I designed a real-time dashboard and integrated REST APIs and MongoDB databases.<br>
+    <p>My third internship was particularly exciting, as I designed a real-time dashboard and integrated REST APIs and MongoDB databases.<br />
     Additionally, I implemented IoT functionality using <code>Mqtt.js</code>, combining both backend and IoT systems seamlessly.
                         </p>
                         <div>
